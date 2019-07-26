62d5a7a3483f54589b5497fd1015249217fdfd33
ツノガエル <ceratophrysgerogero@gmail.com>
2019-07-27 01:02:49 +0900

    ユーザーのメールを参照してgravatarから画像を入手するusersヘルパー作成

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  # 引数で与えられたユーザーのGeavatar画像を返す
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    # 画像データが帰ってくる
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
