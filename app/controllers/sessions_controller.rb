c97e55e9479138c58278b121af928a3bc82f147d
ツノガエル <ceratophrysgerogero@gmail.com>
2019-08-12 16:28:49 +0900

    タイポ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,19 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
-  def create 
+  def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      rediret_to user
+      redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination' 
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
-  
+
   def destroy
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,17 +1,18 @@
 module SessionsHelper
-  # 渡されたユーザーでログイン
+
+  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
 
-  # 現在ログイン中のユーザーを返す
+  # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
-  # ユーザーがログインしていなければtrue
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
