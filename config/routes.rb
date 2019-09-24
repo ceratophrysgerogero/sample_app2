e0daddd0c78de128543468a8e85d61d97646216f
ツノガエル <ceratophrysgerogero@gmail.com>
2019-09-25 03:23:19 +0900

    パスワードリセットのrouteとリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations,only: [:edit]
+  resources :password_reset, only:[:new, :create, :edit, :update]
 end
